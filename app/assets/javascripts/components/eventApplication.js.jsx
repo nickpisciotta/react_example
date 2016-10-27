904dc2a72c25631cac90c24a936c44926226ada6
Nick Pisciotta <npisciotta@gmail.com>
2016-10-27 13:49:37 -0600

    Add working search feature

--- app/assets/javascripts/components/eventApplication.js.jsx
+++ app/assets/javascripts/components/eventApplication.js.jsx
@@ -17,12 +17,20 @@ var EventApplication = React.createClass({
       }
     });
   },
+  handleSearch: function(events) {
+    this.setState({ events: events });
+  },
   render: function() {
     return(
       <div className="container">
         <div className="jumbotron">
           <h1>ReactJS Tutorial</h1>
           <p>by Piotr Jaworski</p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <SearchForm handleSearch={this.handleSearch} />
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
