354d5594cc230f2ae25b0db3ec53f9cc2e9db720
Nick Pisciotta <npisciotta@gmail.com>
2016-10-31 13:36:05 -0600

    Add create action for events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,4 +1,5 @@
   class Api::EventsController < ApplicationController
+
     def index
       render json: Event.all
     end
@@ -10,4 +11,18 @@
       render json: events
     end
 
+    def create
+      event = Event.new(event_params)
+      if event.save
+        render json: event
+      else
+        render nothing: true, status: :bad_request
+      end
+    end
+
+    private
+      def event_params
+        params.require(:event).permit(:name, :description, :event_date
+                                      :place)
+      end
   end
